36179950eaf3977fd7f9e194e0a6439904866992
Kyle Temkin <ktemkin@binghamton.edu>
2016-02-08 00:25:29 -0500

    Add skeleton for Pixel C / smaug.

---
+++ board/nvidia/pixelc/pixelc.c
@@ -0,0 +1,26 @@
+/*
+ * (C) Copyright 2013-2015
+ * NVIDIA Corporation <www.nvidia.com>
+ *
+ * SPDX-License-Identifier:     GPL-2.0+
+ */
+
+#include <common.h>
+#include <i2c.h>
+#include <asm/arch/gpio.h>
+#include <asm/arch/pinmux.h>
+/* #include "../p2571/max77620_init.h" */
+#include "pinmux-config-pixelc.h"
+
+void pin_mux_mmc(void)
+{
+}
+
+/*
+ * Routine: pinmux_init
+ * Description: Do individual peripheral pinmux configs
+ */
+void pinmux_init(void)
+{
+}
+
